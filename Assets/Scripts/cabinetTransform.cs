dffa9847859b6694e0f8e5e8bbc69fdfce99700f
edlyn <edlyn@email.com>
2024-03-26 15:30:15 -0700

    cabinet vectors

--- Assets/Scripts/cabinetTransform.cs
+++ Assets/Scripts/cabinetTransform.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class cabinetTransform : MonoBehaviour
 {
+    private GameObject cabinetDrawer;
+    private Vector3 cabinetDrawerPosition; //variable for position of drawer cabinet
+
+    void Awake()
+    {
+        cabinetDrawer = GameObject.FindWithTag("drawer"); //get drawer
+        cabinetDrawerPosition = gameObject.transform.position; // get drawer position
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Mouse0))
+        
+        //Debug.Log(cabinetDrawerPosition); //print cabinet drawer position
+
+        if (Input.GetKey(KeyCode.Mouse0))
         {
+           
             gameObject.transform.Translate(0, 0, 1);
+            Debug.Log(cabinetDrawerPosition);
+
         }
     }
 }
