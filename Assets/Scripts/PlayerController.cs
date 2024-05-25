697a16980ae15ad89454795342b49b7c20b7bd4e
edlyn <edlyn@email.com>
2024-05-24 23:24:33 -0700

    collider inheritance

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,56 +5,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //private Rigidbody rb;
-    //private float speed;
-    //private Collider objectCollider;
-    private Vector3 centerOfCollider;
-    //private Vector3 endOfCollider;
-    //private Vector2 sizeOfCollider;
-    private Vector3 minOfCollider;
-    private GameObject childObject;
-    private Component parentCoponent;
-    private Collider parentCollider;
-
-     
-    void Start()
+    private Collider p_Collider;
+    private Vector3 p_Vector;
+    parentComponents parentParts; 
+    public void Start()
     {
-        /**this a pushing an on object with an inital force might use but don't delete yet
-        rb = GetComponent<Rigidbody>();
-        rb.AddForce(0f, 0f, 50f); //x y z**/
-
-        //rb = GetComponent<Rigidbody>(); // getting rigid body component
-        //objectCollider = GetComponent<Collider>(); // getting object collider 
-
-        //speed = 5f;
-
-        parentCollider = GetComponentInParent<Collider>();
-        //minOfCollider = parentCollider.bounds.min;
-
-        centerOfCollider = parentCollider.bounds.center;
-        /**endOfCollider = objectCollider.bounds.max;
-        sizeOfCollider = objectCollider.bounds.size;
-        minOfCollider = objectCollider.bounds.min;
-
-        StartCoroutine(velocityRoutine());**/
-        outPutData();
+        p_Collider = GetComponent<Collider>();
+        p_Vector = p_Collider.bounds.center;
     }
-
-    /**IEnumerator velocityRoutine()
-    {
-        rb.velocity = new Vector3(0f, 0f, speed);
-        Debug.Log("about to yield return");
-        yield return null;
-    }**/
 
     void outPutData()
     {
-        Debug.Log("The min of the collider called through the child object " + minOfCollider);
-        Debug.Log("This is the center  " + centerOfCollider);
-        //Debug.Log("This is the size & min of the collider: " +  sizeOfCollider + minOfCollider);
-        //Debug.Log("Object Name: " + parentObject.name);
-     
+        Debug.Log("this is the parent object collider bounds min: " +  p_Vector);
     }
-
-  
 }
--- Assets/Scripts/parentComponents.cs
+++ Assets/Scripts/parentComponents.cs
@@ -2,20 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class parentComponents : MonoBehaviour
+/**Original Collider size for this object is x=20, y=1, z=305 at 0 0 107
+ * for collider testing with parent changed to x=20, y=1, z=50
+ * at center 0 0 230**/
+public class parentComponents : MonoBehaviour 
 {
+    private Collider a_collider; 
+    private Vector3 minVectorCollider;
 
-    public Collider parentCollider;
+    public void Start()
+    {
+        a_collider = GetComponent<Collider>();
 
-    public Collider getCollider()
-    {
-        parentCollider = GetComponent<Collider>();
-        return parentCollider;
+        getCollider(); 
     }
-    public void ParentMethod()
-    {
-        Debug.Log("Parent method called!");
+    public Vector3 getCollider()
+    { 
+        minVectorCollider = a_collider.bounds.min;
+        Debug.Log("Vector from collider: " + minVectorCollider);
+        return minVectorCollider; 
     }
+    /**public void childMovement()
+    {
+        rbChild = GetComponentInChildren<Rigidbody>();
+        colliderChild = GetComponentInChildren<Collider>();
+        Debug.Log("Child collider: " + colliderChild);
+
+    }**/
 
 
     
