f71a03bbc69ae2c2da5dee08397bc5fcd4956b43
edlyn <edlyn@email.com>
2024-05-26 17:28:54 -0700

    velocity and textures

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Collider p_Collider;
-    private Vector3 p_Vector;
-    parentComponents parentParts; 
+    private Rigidbody rb;
+    private Transform position;
+    private float endPosition;
+    
+    
     public void Start()
     {
-        p_Collider = GetComponent<Collider>();
-        p_Vector = p_Collider.bounds.center;
+        rb = GetComponent<Rigidbody>();
+        position = GetComponent<Transform>();
+
+        StartCoroutine(movingForce()); 
     }
 
-    void outPutData()
+    /**public void Update()
     {
-        Debug.Log("this is the parent object collider bounds min: " +  p_Vector);
+        
+        if (position.localPosition.z < 207f)
+        {
+            Debug.Log("Inside if statement");
+            rb.AddForce(0f, 0f, 0f);
+        }
+        else
+        {
+            rb.AddForce(0f, 0f, 5f);
+            Debug.Log("Inside else statment, local position: " + position.localPosition.z);
+        }
+        
+    }**/
+
+    public IEnumerator movingForce()
+    {
+        while (position.localPosition.z > 207f) //while object is not at end position
+        {
+            Debug.Log("Inside while statement");
+            rb.velocity = new Vector3(0f,0f,5f); //put velocity on object 
+            yield return 0;// yield return waits for next frame
+        }
+  
     }
+
 }
