6d0a510b5c7d88a9e4d17f0e72243c760e024c5e
edlyn <edlyn@email.com>
2024-05-29 17:45:29 -0700

    getting physics material

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,12 +9,16 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private Transform position;
     private float endPosition;
+    private Collider colider;
+    private PhysicMaterial physicMaterial;
     
     
     public void Start()
     {
         rb = GetComponent<Rigidbody>();
         position = GetComponent<Transform>();
+        colider = GetComponent<Collider>();
+        physicMaterial = colider.material;
 
         StartCoroutine(movingForce()); 
     }
@@ -35,12 +39,21 @@ public class PlayerController : MonoBehaviour
         
     }**/
 
+    public int Velocity()
+    {
+
+
+        return 0; 
+    }
+
     public IEnumerator movingForce()
     {
         while (position.localPosition.z > 207f) //while object is not at end position
         {
             Debug.Log("Inside while statement");
-            rb.velocity = new Vector3(0f,0f,5f); //put velocity on object 
+            rb.velocity = new Vector3(0f,0f,5f); //put velocity on object
+            Debug.Log("Velocity of object: " + rb.velocity);
+            Debug.Log("Friction of track and object:" + physicMaterial.name);
             yield return 0;// yield return waits for next frame
         }
   
